693152f7a8d9dda5c2023306fc4eeae8d044d8a2
Vaquita Marina <cristhianvm55@gmail.com>
2024-03-16 02:15:13 -0500

    add keymap

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,85 @@
-import { Github, Youtube, Palette, Facebook, Bot } from "lucide-react"
-import { useState } from 'react'
-import { Header } from './Components/Header.jsx'
-import { Card } from './Components/Card.jsx'
-import { Clock } from './Components/Clock.jsx'
-import { Weather } from "./Components/Weather.jsx"
+import { Github, Youtube, Palette, Facebook, Bot } from "lucide-react";
+import { useEffect } from "react";
+import { Header } from "./Components/Header.jsx";
+import { Card } from "./Components/Card.jsx";
+import { Clock } from "./Components/Clock.jsx";
+import { Weather } from "./Components/Weather.jsx";
 
-
-
-import './App.css'
-
+import "./App.css";
 function App() {
-    return (
+  // useEffect(() => {
+  //   const keyDownHandler = (event) => {
+  //     console.log("key pressed:" + event.key);
+  //   };
+  //   window.addEventListener("keydown", keyDownHandler);
+  //   return () => {
+  //     window.removeEventListener("keydown", keyDownHandler);
+  //   };
+  // }, []);
+  return (
     <>
       <Header></Header>
-      <div className='wrap'>
-            <div className='grid1'>
-            <Card title={''} href={'https://github.com/vaquitamarina'} >             
-                
-                <Github size={48}/>
-            </Card>
-            </div>
+      <div className="wrap">
+        <div className="grid1">
+          <Card title={""} href={"https://github.com/vaquitamarina"} keyId={""}>
+            <Github size={48} />
+          </Card>
+        </div>
 
-            <div className='grid2'>
-            <Card title={''} href={''} >
-                <Bot size={48}/>
-            </Card>
-            </div>
-            
-            {/* <div className='grid3'>
-            <Card title={''} href={'https://coolors.co/generate'} >
-                <Palette size={48}/>
-            </Card>
-            </div>
-            
-            <div className='grid4'>
-            <Card title={''} href={'https://coolors.co/generate'} >
-                <Palette size={48}/>
-            </Card>
-            </div> */}
+        <div className="grid2">
+          <Card title={""} href={"https://chat.openai.com/"} keyId={"c"}>
+            <Bot size={48} />
+          </Card>
+        </div>
 
-            <div className='grid5'>
-            <Card title={''} href={'https://github.com/vaquitamarina'} >
-                <Github size={48}/>
-            </Card>
-            </div>
+        <div className="grid5">
+          <Card
+            title={""}
+            href={"https://github.com/vaquitamarina"}
+            keyId={"g"}
+          >
+            <Github size={48} />
+          </Card>
+        </div>
 
-            <div className='grid6'>
-            <Card title={''} href={'https://www.youtube.com/'} >
-                <Youtube size={48}/>
-            </Card>
-            </div>
+        <div className="grid6">
+          <Card title={""} href={"https://www.youtube.com/"} keyId={"y"}>
+            <Youtube size={48} />
+          </Card>
+        </div>
 
-            <div className='grid7'>
-            <Card title={''} href={'https://coolors.co/generate'} >
-                <Weather></Weather>
-            </Card>
-            </div>
+        <div className="grid7">
+          <Card title={""} href={"https://coolors.co/generate"} keyId={"e"}>
+            <Weather></Weather>
+          </Card>
+        </div>
 
-            <div className='grid8'>
-            <Card title={''} href={'https://coolors.co/generate'} >
-                <Palette size={48}/>
-            </Card>
-            </div>
+        <div className="grid8">
+          <Card title={""} href={"https://coolors.co/generate"} keyId={"h"}>
+            <Palette size={48} />
+          </Card>
+        </div>
 
-            <div className='grid9'>
-            <Card title={''} href={'https://www.facebook.com/'} >
-                <Facebook size={48}/>
-            </Card>
-            </div>
+        <div className="grid9">
+          <Card title={""} href={"https://www.facebook.com/"} keyId={"f"}>
+            <Facebook size={48} />
+          </Card>
+        </div>
 
-            <div className='grid10'>
-            <Card title={''} href={'https://coolors.co/generate'} >
-                <Clock></Clock>
-            </Card>
-            </div>
+        <div className="grid10">
+          <Card title={""} href={"https://coolors.co/generate"}>
+            <Clock></Clock>
+          </Card>
+        </div>
 
-            <div className='grid11'>
-            <Card title={''} href={'https://coolors.co/generate'} >
-                <Palette size={48}/>
-            </Card>
-            </div>
-
-            
+        <div className="grid11">
+          <Card title={""} href={"https://coolors.co/generate"}>
+            <Palette size={48} />
+          </Card>
+        </div>
       </div>
     </>
-
-    )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,22 @@
-
-
-export function Card({title, href,children}) {
-    return (
-        <a className="card" href={href}>
-            <div>
-                <h2>
-                    {title}
-                </h2>
-                {children}
-            </div>
-        </a>
-    )
+import { useEffect } from "react";
+export function Card({ title, href, children, keyId }) {
+  useEffect(() => {
+    const keyDownHandler = (event) => {
+      if (event.key == keyId) {
+        window.open(href, "_blank");
+      }
+    };
+    window.addEventListener("keydown", keyDownHandler);
+    return () => {
+      window.removeEventListener("keydown", keyDownHandler);
+    };
+  }, []);
+  return (
+    <a className="card" target="_blank" href={href}>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+    </a>
+  );
 }
